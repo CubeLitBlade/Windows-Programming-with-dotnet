4b2cf72f5026468c26fdb2c7f0f9d8ed38fcc2d0
Ray Leung <135247075+CubeLitBlade@users.noreply.github.com>
2024-03-18 11:18:29 +0800

    🎈 perf: Improve print format
    - Now  the Pascal's Triangle can be printed in accordance with the known method if the width permits.
    
    Co-authored-by: @25KStars <162898561+25KStars@users.noreply.github.com>

--- 1-6/Program.cs
+++ 1-6/Program.cs
@@ -4,6 +4,8 @@ namespace _1_6
 {
     internal class Program
     {
+        public const int width = 3;    //< 定义杨辉三角数与数之间的宽度。
+
         static void Main(string[] args)
         {
             // 调用方法打印索引到 10 的杨辉三角。
@@ -18,17 +20,16 @@ namespace _1_6
         /// </summary>
         private static void PrintPascalTriangle(uint n)
         {
-            for (uint i = 0; i <= n; i++)
+            for (uint rowIndex = 0; rowIndex <= n; ++rowIndex)
             {
-                // 添加空格用于打印居中的杨辉三角。
-                // 由于未能解决杨辉三角无法对齐的问题，暂时弃用。
-                // Console.Write("".PadLeft((int)(n - i)));
+                // 添加空格用于打印居中的杨辉三角。此方案由 @25KStars 完善。
+                Console.Write("".PadRight((int)(n - rowIndex) * width));
 
                 // 打印杨辉三角的数值段内容。
-                for (uint j = 0; j <= i; j++)
+                for (uint colIndex = 0; colIndex <= rowIndex; colIndex++)
                 {
                     // 杨辉三角的各个元素是相应位置的组合数。
-                    Console.Write($"{GetCombinatorialNumber(j, i)}\t");
+                    Console.Write($"{GetCombNum(colIndex, rowIndex), -2 * width}");
                 }
                 Console.WriteLine();
             }
@@ -38,7 +39,7 @@ namespace _1_6
         /// 获取组合数 C(n, m) 的值。
         /// </summary>
         /// <returns>若 n <= m，返回C(n, m)的值；否则返回 0.</returns>
-        private static uint GetCombinatorialNumber(uint n, uint m)
+        private static uint GetCombNum(uint n, uint m)
         {
             if (n <= m)
             {
